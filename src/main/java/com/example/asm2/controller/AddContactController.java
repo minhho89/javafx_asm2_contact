3a5fe00d25b7e69fceed5134cfaa9d136185bb3a
minhho <minh.ho89@gmail.com>
2021-08-26 12:23:28 +0700

    initial commit

---
+++ src/main/java/com/example/asm2/controller/AddContactController.java
@@ -0,0 +1,32 @@
+package com.example.asm2.controller;
+
+import com.example.asm2.entity.Contact;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.util.List;
+
+public class AddContactController {
+
+    public void  setAddContactController(ContactController contactController) {
+        throw new UnsupportedOperationException("Remove this line and implement your code here!");
+    }
+
+    private List<Contact> contacts;
+
+    public  void setContacts(List<Contact> contacts) {
+        this.contacts = contacts;
+    }
+
+    @FXML
+    void initialize()throws  Exception {
+        throw new UnsupportedOperationException("Remove this line and implement your code here!");
+    }
+
+    public  void saveContact() throws Exception {
+        throw new UnsupportedOperationException("Remove this line and implement your code here!");
+    }
+    public  void saveContact(ActionEvent evt)throws  Exception {
+        throw new UnsupportedOperationException("Remove this line and implement your code here!");
+    }
+}
---
+++ src/main/java/com/example/asm2/controller/ContactController.java
@@ -0,0 +1,48 @@
+package com.example.asm2.controller;
+
+import com.example.asm2.entity.Contact;
+import com.example.asm2.entity.Group;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.util.List;
+
+
+public class ContactController {
+
+
+    @FXML
+    void initialize() {
+        throw new UnsupportedOperationException("Remove this line and implement your code here!");
+    }
+
+    //output all contact to tblContact
+    public  void showContact(List<Contact> c) {
+        throw new UnsupportedOperationException("Remove this line and implement your code here!");
+    }
+    //output all groups to dropdownlist
+    public  void showGroup(List<Group> g) {
+        throw new UnsupportedOperationException("Remove this line and implement your code here!");
+    }
+    //do corresponding actions for search, delete, update and add contact
+    public void searchContact(ActionEvent evt) throws Exception{
+        throw new UnsupportedOperationException("Remove this line and implement your code here!");
+
+    }
+    //manage the groups
+    public void groupPanel() throws Exception{
+        throw new UnsupportedOperationException("Remove this line and implement your code here!");
+
+    }
+
+    //update a contact
+    public  void updateContact()throws Exception {
+        throw new UnsupportedOperationException("Remove this line and implement your code here!");
+    }
+    //delete a selected contact
+    public  void deleteContact()throws  Exception {
+        throw new UnsupportedOperationException("Remove this line and implement your code here!");
+    }
+
+
+}
